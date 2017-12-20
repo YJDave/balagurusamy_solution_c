f43949da97a061f625dd090c648bf1053ed4d70d
anerimehta <aneri1297@gmail.com>
2017-12-20 11:52:35 +0530

    1: Solution for 1.2

---
+++ Exercise_1/1.2_Sol.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main()
+{
+	printf("---------------------------------------\n");
+	printf("|First line     :  Sherlock Holmes    |\n|Second line    :  221 B, Baker Street|\n|Third line     :  London             |\n");
+	printf("---------------------------------------\n");
+
+}
