7f280863b1f957650d68df865f44ba783caaa795
anerimehta <aneri1297@gmail.com>
2017-12-20 11:59:31 +0530

    1: Solution for 1.14

---
+++ Exercise_1/1.14_Sol.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+
+void main()
+{
+	int a,b,c;
+	printf("Enter a, b, c");
+	scanf("%d%d%d",&a,&b,&c);
+	printf("The equation of the line is: %dx + %dy = %d",a,b,c);
+}
