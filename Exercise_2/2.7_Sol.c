73f79f99a892d960f6c1c406576d627e5c11b25a
anerimehta <aneri1297@gmail.com>
2017-12-20 16:04:11 +0530

    2: Solution for 2.7

---
+++ Exercise_2/2.7_Sol.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+	int x,y;
+	short z;
+	x = 343534;
+	y = 283729;
+	z = x+y;
+	printf("The values of x, y and z are: %d %d %i\n",x,y,z);
+	printf("Size of an integer is 16bits and for short is 8 bits.");
+}
