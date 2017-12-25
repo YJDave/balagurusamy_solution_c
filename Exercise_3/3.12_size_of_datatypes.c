01bf9d2bff792ce554cd9523bc1f2ecd14398c64
BansiJoshi <bkjoshi14297@gmail.com>
2017-12-25 18:42:54 +0530

    3: Add solution of 3.12

---
+++ Exercise_3/3.12_size_of_datatypes.c
@@ -0,0 +1,10 @@
+//Write a program to print the size of various data types in C.
+
+#include<stdio.h>
+void main()
+{
+    printf("Size of Character Data Type : %ld \n",sizeof(char));
+    printf("Size of Integer Data Type : %ld \n",sizeof(int));
+    printf("Size of Float Data Type : %ld \n",sizeof(float));
+    printf("Size of Double Data Type : %ld \n",sizeof(double));
+}
